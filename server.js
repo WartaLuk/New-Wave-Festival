f52a90806cf302a896553cbc74ccddace1d13cbb
Łukasz Wartałowicz <wartalowicz.lukasz.praca@gmail.com>
2022-11-11 07:11:47 +0100

    Add endpoints for concerts

--- server.js
+++ server.js
@@ -79,9 +79,40 @@ app.put("/seats/:id", (req, res) => {
   res.json({ message: "OK" });
 });
 
-// app.ge
-
 //CONCERTS
+app.get("/concerts", (req, res) => {
+  res.json(concerts);
+});
+
+app.get("/concerts/:id", (req, res) => {
+  res.json(concerts[req.params.id - 1]);
+});
+
+app.post("/concerts", (req, res) => {
+  const { performer, genre, price, day, image } = req.body;
+  const id = randomID();
+  const newConcert = { id, performer, genre, price, day, image };
+  concerts.push(newConcert);
+  res.json({ message: "OK" });
+});
+
+app.delete("/concerts/:id", (req, res) => {
+  const id = req.params.id - 1;
+  testimonials.splice(concerts[id], 1);
+  res.json({ message: "OK" });
+});
+
+app.put("/concerts/:id", (req, res) => {
+  const { performer, genre, price, day, image } = req.body;
+  const id = req.params.id - 1;
+  const concert = concerts[id];
+  concert.performer = performer;
+  concert.genre = genre;
+  concert.price = price;
+  concert.day = day;
+  concert.image = image;
+  res.json({ message: "OK" });
+});
 
 //REST
 app.use((req, res) => {
