88678d9fc123dff5d177f532463a9e05e361826b
Łukasz Wartałowicz <wartalowicz.lukasz.praca@gmail.com>
2022-11-23 11:04:11 +0100

    Update type from Date to Number in concert & seat schema

--- models/seat.model.js
+++ models/seat.model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const seatSchema = new mongoose.Schema({
-  day: { type: Date, required: true },
+  day: { type: Number, required: true },
   seat: { type: Number, required: true },
   client: { type: String, required: true },
   email: { type: String, required: true },
