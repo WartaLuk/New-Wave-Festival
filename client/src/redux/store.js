0e5721a23c08f8ab0c87c2e31c95792fd6c6aefb
Lukasz <100516000+WartaLuk@users.noreply.github.com>
2022-11-30 21:36:29 +0000

    Remove devtools

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -13,10 +13,8 @@ const rootReducer = combineReducers({
 
 const store = createStore(
   rootReducer,
-  compose(
 		applyMiddleware(thunk),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
+	
 );
 
 export default store;
